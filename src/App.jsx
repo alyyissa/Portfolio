ef53742e62cf65be265af12037064a9161a49b0c
Ali Issa <alyy.issa01@gmail.com>
2026-01-10 15:58:54 +0200

    Added the navbar and the colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <Route
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='h-14 bg-background text-primary flex items-center px-4'>
+    <div className='bg-background text-primary flex items-center px-4 h-24 fixed top-0 w-full'>
       
     </div>
   )
