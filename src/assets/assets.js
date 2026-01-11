9f74349f22a4ff9bd975adba3d3de2dad0fa025b
Ali Issa <alyy.issa01@gmail.com>
2026-01-11 14:51:04 +0200

    Added the navbar and finished it

---
+++ src/assets/assets.js
@@ -0,0 +1,6 @@
+import logo from './images/logo2.png'
+
+export const assets = { 
+    logo,
+
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div className='bg-background text-primary px-4 h-24 fixed top-0 w-full flex flex-between'>
-      <img src="" alt="" />
-      
+    <div className='bg-background text-primary h-24 fixed top-0 w-full flex justify-between flex-row items-center
+    px-3 sm:px-4 md:px-11 lg:px-13 xl:px-12 2xl:px-16'>
+      <img src={assets.logo} alt="" />
+        <ul className='flex flex-row gap-12'>
+          <li>Home</li>
+          <li>About</li>
+          <li>My Work</li>
+        </ul>
+      <div>
+        <button className='bg-primary py-3 px-8 rounded text-background font-semibold cursor-pointer'>Contact Me</button>
+      </div>
     </div>
   )
 }
