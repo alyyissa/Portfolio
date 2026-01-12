6ec8d6fb2dad2ca7b03188bd8b5bed84bb5351a1
Ali Issa <alyy.issa01@gmail.com>
2026-01-12 22:47:44 +0200

    Added the styles for the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
     px-3 sm:px-4 md:px-11 lg:px-13 xl:px-12 2xl:px-16'>
       <img src={assets.logo} alt="" />
         <ul className='flex flex-row gap-12'>
-          <li>Home</li>
-          <li>About</li>
-          <li>My Work</li>
+          <li className='cursor-pointer'>Home</li>
+          <li className='cursor-pointer'>About</li>
+          <li className='cursor-pointer'>My Work</li>
         </ul>
       <div>
         <button className='bg-primary py-3 px-8 rounded text-background font-semibold cursor-pointer'>Contact Me</button>
