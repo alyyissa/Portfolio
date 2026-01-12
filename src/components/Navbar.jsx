f431f074990418831bfaa3c9fc31315e1540422f
Ali Issa <alyy.issa01@gmail.com>
2026-01-12 22:52:00 +0200

    Fixed some styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div className='bg-background text-primary h-24 fixed top-0 w-full flex justify-between flex-row items-center
+    <div className='bg-background text-primary h-24 fixed top-0 w-full flex justify-center md:justify-between flex-row items-center
     px-3 sm:px-4 md:px-11 lg:px-13 xl:px-12 2xl:px-16'>
-      <img src={assets.logo} alt="" />
-        <ul className='flex flex-row gap-12'>
-          <li className='cursor-pointer'>Home</li>
-          <li className='cursor-pointer'>About</li>
-          <li className='cursor-pointer'>My Work</li>
-        </ul>
-      <div>
+      <img src={assets.logo} alt="Logo" />
+      <ul className=' flex-row gap-12 md:flex hidden'>
+        <li className='cursor-pointer'>Home</li>
+        <li className='cursor-pointer'>About</li>
+        <li className='cursor-pointer'>My Work</li>
+      </ul>
+      <div className='md:block hidden'>
         <button className='bg-primary py-3 px-8 rounded text-background font-semibold cursor-pointer'>Contact Me</button>
       </div>
     </div>
