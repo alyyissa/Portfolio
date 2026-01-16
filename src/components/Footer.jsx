b85c6f605fc44d5ea8d50c7b2be15189c5f85686
Ali Issa <alyy.issa01@gmail.com>
2026-01-16 18:33:40 +0200

    Added the hero positioning and the footer to the home screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    
-
-<footer class="bg-neutral-primary-soft rounded-base shadow-xs border border-default m-4">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-body sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+<footer class="bg-background rounded-base shadow-xs border border-default py-4">
+    <div class="w-full mx-auto max-w-7xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-body sm:text-center text-primary">© <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
     </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-body sm:mt-0">
+    <ul class="flex flex-wrap items-center mt-3 text-primary text-sm font-semibold text-body sm:mt-0">
         <li>
             <a href="#" class="hover:underline me-4 md:me-6">About</a>
         </li>
@@ -25,7 +23,7 @@ const Footer = () => {
     </div>
 </footer>
 
-  )
+)
 }
 
 export default Footer
